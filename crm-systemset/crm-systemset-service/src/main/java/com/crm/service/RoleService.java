406eb25b7f094d8243983ec9d9ed5521267ce7f0
wangzh <wangzh951219@163.com>
2020-03-10 09:15:41 +0800

    搭建项目

---
+++ crm-systemset/crm-systemset-service/src/main/java/com/crm/service/RoleService.java
@@ -0,0 +1,29 @@
+package com.crm.service;
+
+import com.crm.bean.Role;
+import com.crm.bean.RoleExample;
+import com.crm.enums.ExceptionEnums;
+import com.crm.exception.CrmException;
+import com.crm.mapper.RoleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    public Role findById(Long id){
+        if(id == null) {
+            throw new CrmException(ExceptionEnums.PARAM_IS_NULL);
+        }
+        return roleMapper.selectByPrimaryKey(id);
+    }
+
+    public List<Role> findAllRole() {
+        return roleMapper.selectByExample(new RoleExample());
+    }
+}
